2d88be26c0fb2bd4add14d58d0d2e2057b8f9bc8
Dourado_root <contato@dfsweb.com.br>
2020-09-27 13:03:40 -0300

    No cabecalho coloca o content-type de json com charset utf-8

--- src/services/apiFilmes.js
+++ src/services/apiFilmes.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-
+        'content-type': 'application/json;charset-utf-8'
     }
 })
 
