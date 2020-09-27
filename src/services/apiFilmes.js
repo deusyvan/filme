84e928441e85e7b36b0afc5e68b7947c53cbe9f3
Dourado_root <contato@dfsweb.com.br>
2020-09-27 13:07:49 -0300

    Inserindo a autorizaçao com sua api-key

--- src/services/apiFilmes.js
+++ src/services/apiFilmes.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
-        'content-type': 'application/json;charset-utf-8'
+        'content-type': 'application/json;charset-utf-8',
+        'Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTAwYjZhNzA0OGE1NjM2NmJhNWI0MmQ2ZmVlYjQ5ZSIsInN1YiI6IjVmNzA5YTQzM2RkMTI2MDAzNzY1MzI0ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZWL-eEisqeS0qXdldfEz6_uHNl8IBqRRODbc-_-fgxg'
     }
 })
 
