691456fec28db7ca53a21e85a632b3d09fe86a0c
Dourado_root <contato@dfsweb.com.br>
2020-09-27 13:24:13 -0300

    Criando uma constante de filmes

--- src/screens/filmes/FilmesPopulares.jsx
+++ src/screens/filmes/FilmesPopulares.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export default () => {
+
+const filmes = ['Filme 1', 'Filme 2','Filme 3']
+
   return (
     <>
         <View>
